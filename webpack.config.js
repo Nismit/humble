a5b825ed046776a21a5dd70404b555d55cf6115c
git <nismit.dev@gmail.com>
2019-05-01 21:55:05 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => ({
+  entry: { 
+    bundle: './src/js/index.js'
+  },
+
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'static/js/')
+  },
+
+  devtool: argv.mode === 'production' ? 'none' : 'source-map',
+
+  performance: {
+    maxEntrypointSize: 1000000
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'static'),
+    compress: true,
+    port: 8080
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: argv.mode === 'production' ? false : true,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: argv.mode === 'production' ? false : true,
+              plugins: [
+                // https://github.com/postcss/autoprefixer#options
+                require('autoprefixer')({
+                  grid: true, // CSS Grid Layout
+                  'browsers': [
+                    'IE 10'
+                  ]
+                })
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: argv.mode === 'production' ? false : true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { 'modules': 'commonjs' }]  // "modules" true, it will be converted to CommonJS
+              ]
+            }
+          }
+        ],
+        exclude: /node_modules/,
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '../css/style.css'
+    })
+  ]
+});
